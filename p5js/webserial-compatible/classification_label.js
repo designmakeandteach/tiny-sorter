77234acb4ed7b34efb7cfef86c36ee92cb6b1452
Eric Ayers <ericzundel@gmail.com>
2025-07-02 11:20:30 -0400

    Removed Poppins font, added in updated classification_label.js

---
+++ p5js/webserial-compatible/classification_label.js
@@ -0,0 +1,131 @@
+/**
+ * A p5 element that renders around the classification label when a classification is detected.
+ * 
+ * @param {boolean} isLeft - Whether the splash screen is on the left or right side of the screen.
+ */
+class Splash {
+  constructor(x, y, width, height, isLeft) {
+
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.isLeft = isLeft;
+
+    this.color = color(22, 79, 200);
+    this.isExploding = false;
+    this.isInbetweenUpdates = false;
+    this.explosionRadius = 100;
+    this.explosionIndex = 0;
+    this.numRadius = 4;
+    this.radiusOffset = 10;
+
+  }
+
+  trigger() {
+    this.isExploding = true;
+  }
+
+  updateIndex() {
+    this.explosionIndex++;
+    this.isInbetweenUpdates = false;
+  }
+
+  render() {
+    if (!this.isExploding) {
+      fill(this.color);
+      // rect(this.x, this.y, this.width, this.height);
+    } else {
+      noFill();
+      strokeWeight(3);
+      stroke(this.color);
+      rect(
+        this.x,
+        this.y,
+        this.width + this.radiusOffset * this.explosionIndex,
+        this.height + this.radiusOffset * this.explosionIndex,
+        9,
+        9,
+        9,
+        9
+      );
+    }
+
+    if (this.isExploding && !this.isInbetweenUpdates) {
+      setTimeout(() => {
+        this.updateIndex();
+      }, 100);
+      this.isInbetweenUpdates = true;
+    }
+
+    if (this.explosionIndex >= this.numRadius) {
+      this.isExploding = false;
+      this.isInbetweenUpdates = false;
+      this.explosionIndex = 0;
+    }
+  }
+}
+
+/**
+ * A p5 element that renders the classification label.
+ */
+class ClassificationLabel {
+  constructor(x, y, width, height, radius, isLeft) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.radius = radius;
+    this.isLeft = isLeft;
+    this.text_value = "";
+    this.is_visible = false;
+  }
+
+  /**
+   * Set the value of the classification label.
+   * @param {string} value - The value to display in the element. 
+   */
+  value(value) {
+    this.text_value = value;
+  }
+
+  /**
+   * Set the visibility of the classification label.
+   * @param {boolean} visible - Whether the element is visible.
+   */
+  visible(visible) {
+    this.is_visible = visible;
+  }
+
+  /**
+   * Render the classification label.
+   */
+  render() {
+    if (this.is_visible) {
+      fill(255);
+      rectMode(CENTER);
+      noStroke();
+      rect(
+        this.x,
+        this.y,
+        this.width,
+        this.height,
+        this.radius,
+        this.radius,
+        this.radius,
+        this.radius
+      );
+
+      fill("#1967D2");
+      textSize(24);
+      textStyle(BOLD);
+      if (this.isLeft) {
+        textAlign(LEFT, CENTER);
+        text(this.text_value, this.x - this.width / 2 + 10, this.y );
+      } else {
+        textAlign(RIGHT, CENTER);
+        text(this.text_value, this.x + this.width / 2 - 13, this.y );
+      }
+    }
+  } // end render()
+}
