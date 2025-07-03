dd5dfa2d3f31c29e323fdb5940e8568796c0b65c
Eric Ayers <ericzundel@gmail.com>
2025-07-03 09:47:02 -0400

    Zundel/circuit python driver (#15)
    * First version of Circuit Python Driver

---
+++ circuitpython/README.md
@@ -0,0 +1,74 @@
+# CircuitPython driver for Tiny Sorter
+
+This is a driver for the [Tiny Sorter](https://designmakeandteach.com/projects/tiny-sorter) project.
+
+It communicates with a web page running using WebSerial.
+
+
+## Installing
+
+1. Copy `code.py` to the CIRCUITPY drive on your computer.
+2. Find the Adafruit Circuit Python Bundle for the version of CircuitPython
+installed on your microcontroller. 
+3. Copy the folder `lib/adafruit_motor` to the `lib` folder on 
+the CIRCUITPY drive. Download from the 
+[CircuitPython Libraries](https://circuitpython.org/libraries) website.
+4. Attach your servo and test. You may need to modify this line of
+code to match the pin you are using for the servo:
+
+```Python
+SERVO_PIN = board.GP2
+```
+
+## Tested Devices
+
+This was tested with CircuitPython 9.x on the following devices:
+
+- Raspberry Pi Pico W
+- Raspberry Pi Pico 2
+
+
+## Troubleshooting
+
+### Servo doesn't move
+Make sure your pins are connected correctly to the Servo
+
+Servo Wire Color:
+
+- Red: +5V Power (labled VCC, VBUS or VSYS on most devices)
+- Black: 0V Ground (labeled GND on most devices)
+- White: Signal (pin set with SERVO_PIN in code.py)
+
+### Website doesn't see the device or reports an error
+
+Only one program can access the serial port at a time.  Try 
+closing the serial port monitor from your IDE (Thonny or Mu Editor)
+or exiting the IDE all together, then try to reconnect.
+
+### Servo moves at the beginning, but then stops
+
+This could be a problem with the code.  Use [Thonny](https://thonny.org/) to
+look for a message from the serial console.
+
+You may see an error from Python about select() not 
+being supported.  Try changing this line of code:
+
+```Python
+USE_SELECT = True
+```
+
+to read
+
+```Python
+USE_SELECT = False
+```
+
+### Error from console: object has no attribute 'in_waiting'
+
+Try setting `USE_SELECT` to `True` in code.py
+
+
+## Contributing and Feedback
+
+See the Design, Make, and Teach [github repository](https://github.com/designmakeandteach/tiny-sorter)
+to report any issues or submit a patch. 
---
+++ circuitpython/code.py
@@ -0,0 +1,141 @@
+# CircuitPython Driver for Tiny Sorter project
+# Copyright 2025 Eric Z. Ayers
+#
+# See https://github.com/designmakeandteach/tiny-sorter
+#
+# This driver waits on a command from the Serial port.
+# Messages are single digits. By default, these messages are:
+#
+# '1' == A piece of cereal was detected
+# '2' == A marshmallow was detected
+#
+# Once it arrives, it moves the servo to the specified position.
+# Between messages it will move the servo to vibrate the sorter
+
+import board
+import pwmio
+import select
+import sys
+import time
+
+# Install adafruit_motor in the lib/ directory
+# You can download it from https://circuitpython.org/libraries
+from adafruit_motor import servo
+
+# ACTION REQUIRED: Update SERVO_PIN to match your microprocessor
+# This pin must be attached to a PWM capable port
+
+# Raspberry Pi Pico has many pins that can do PWM
+SERVO_PIN = board.GP2
+
+# On some systems, you may get an error trying to read a character with
+# select(). You can change this value to Falst to try an alternative version
+USE_SELECT = True
+
+# Message values sent from the webpage
+CEREAL_VALUE = "1"
+MALLOW_VALUE = "2"
+
+# Angles to use to move the servo after an item is detected
+CENTER_POSITION = 90
+CEREAL_POSITION = 10
+MALLOW_POSITION = 170
+
+# Controls the vibration of the servo betwene pieces
+NUM_JIGGLES = 4
+JIGGLE_ANGLE = 15
+JIGGLE_DELAY = 0.1
+
+# -------------------------------------------------------------------------
+# Global Variables
+# -------------------------------------------------------------------------
+
+pwm = pwmio.PWMOut(SERVO_PIN, duty_cycle=2**15, frequency=50)
+sorter_servo = servo.Servo(pwm)
+
+# -------------------------------------------------------------------------
+# Helper routines
+# -------------------------------------------------------------------------
+
+
+# Attempts to read a command consisting of a digit
+# Discards all but the last digit read.
+# If no input is available, returns None
+def read_command():
+    if USE_SELECT:
+        return read_command_nonblocking_select()
+    else:
+        return read_command_nonblocking_no_select()
+
+
+# Moves the servo back and forth to vibrate the sorter and move the
+# items down the chute.
+def jiggle():
+    print("Jiggle!")
+    for i in range(0, NUM_JIGGLES):
+        sorter_servo.angle = CENTER_POSITION + JIGGLE_ANGLE
+        time.sleep(JIGGLE_DELAY)
+        sorter_servo.angle = CENTER_POSITION - JIGGLE_ANGLE
+        time.sleep(JIGGLE_DELAY)
+    time.sleep(JIGGLE_DELAY)
+
+
+# Helper routine to move the sorter and give the item time to fall
+def move_to_position_and_wait(angle, timeout=2):
+    sorter_servo.angle = angle
+    time.sleep(timeout)
+
+
+# After receivng a command to dump the sorter, moves into
+# that position, then re-centers the servo and prepares
+# for the next command
+def dump_sorter(angle):
+    move_to_position_and_wait(angle)
+    move_to_position_and_wait(CENTER_POSITION, 1)
+    # Throwaway any data that's come while we have been waiting
+    throwaway = read_command()
+
+
+# Read from the serial port without blocking.
+# On some CircuitPython installations select() may not be available
+def read_command_nonblocking_no_select():
+    val = None
+    if sys.stdin.in_waiting > 0:
+        # Read all available bytes
+        raw_input = sys.stdin.read(sys.stdin.in_waiting)
+        # We only want the last valid command, throw the rest away
+        for tmp_val in list(raw_input):
+            if tmp_val.isdigit():
+                val = tmp_val
+    return val
+
+
+# A version that reads a character non-blocking using select()
+def read_command_nonblocking_select():
+    val = None
+    while select.select([sys.stdin], [], [], 0)[0]:
+        tmp_val = sys.stdin.read(1)
+        if tmp_val.isdigit():
+            val = tmp_val
+    return val
+
+
+# -------------------------------------------------------------------------
+# Main Logic
+# -------------------------------------------------------------------------
+
+print("Waiting for data")
+move_to_position_and_wait(CENTER_POSITION)
+
+while True:
+    # Read a single digit command from the input
+    command = read_command()
+    print("Received: input: ", command)
+    if command == CEREAL_VALUE:
+        print("Cereal Detected")
+        dump_sorter(CEREAL_POSITION)
+    elif command == MALLOW_VALUE:
+        print("Mallow Detected")
+        dump_sorter(MALLOW_POSITION)
+    else:
+        jiggle()
